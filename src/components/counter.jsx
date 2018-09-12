d65036180af1e29cca59967d9ad54f5e3edcc4c7
akash jain <akjain@grepruby.com>
2018-09-12 18:04:54 +0530

    Added Delete and Reset button to APP

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,11 +1,5 @@
 import React, { Component } from "react";
 class Counter extends Component {
-  state = {
-    value: this.props.value,
-    tags: []
-    //    imageUrl: "https://picsum.photos/200"
-  };
-
   // styles = {
   //   fontSize: 150,
   //   fontWeigth: "bold"
@@ -16,13 +10,9 @@ class Counter extends Component {
   //   this.handleIncrement = this.handleIncrement.bind(this);
   // }
 
-  handleIncrement = product => {
-    this.setState({ value: this.state.value + 1 });
-  };
-
-  handleDecrement = () => {
-    this.setState({ value: this.state.value - 1 });
-  };
+  // handleDecrement = () => {
+  //   this.setState({ value: this.state.value - 1 });
+  // };
 
   render() {
     return (
@@ -30,20 +20,23 @@ class Counter extends Component {
         <span style={{ fontSize: 20 }} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
+
         <button
-          onClick={() => this.handleIncrement({ id: 1 })}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btm-sm"
         >
           Increment
         </button>
+
         <button
-          onClick={this.handleDecrement}
+          onClick={() => this.props.onDecrement(this.props.counter)}
           className="btn btn-warning btn-sm m-2"
         >
           Decreament
         </button>
+
         <button
-          onClick={() => this.props.onDelete(this.props.id)}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
           className="btn btn-danger btn-sm m-2"
         >
           Delete
@@ -54,12 +47,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? value : value;
   }
 }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,25 +3,57 @@ import Counter from "./counter";
 class Counters extends Component {
   state = {
     counters: [
-      { id: 1, value: 5 },
-      { id: 2, value: 4 },
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
       { id: 3, value: 0 },
       { id: 4, value: 0 }
     ]
   };
-  handleDecrement = counterId => {
+  handleDelete = counterId => {
     const counters = this.state.counters.filter(c => c.id !== counterId);
     this.setState({ counters: counters });
   };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    //counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters: counters });
+  };
+
+  handleDecrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index].value--;
+    this.setState({ counters: counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters: counters });
+  };
+
   render() {
     return (
       <div>
+        <button
+          onClick={() => this.handleReset()}
+          className="btn btn-primary btm-sm m-5"
+        >
+          Reset
+        </button>
+        <p>There are {this.state.counters.length} Items in Cart.</p>
         {this.state.counters.map(counter => (
           <Counter
             key={counter.id}
-            id={counter.id}
-            onDelete={this.handleDecrement}
-            value={counter.value}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+            counter={counter}
           /> //child component ==Counter and props are value,onDelete
         ))}
       </div>
